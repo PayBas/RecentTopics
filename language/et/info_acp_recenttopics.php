c5c28f3156c36b987874dddc943f0f703c9e0835
phpBBeesti <info@phpbbeesti.com>
2014-10-01 18:46:48 +0300

    Update info_acp_recenttopics.php

--- language/et/info_acp_recenttopics.php
+++ language/et/info_acp_recenttopics.php
@@ -30,11 +30,11 @@ $lang = array_merge($lang, array(
 	'RT_NUMBER'						=> 'Hiljutised teemad',
 	'RT_NUMBER_EXP'					=> 'Esilehel näidatavate teemade arv.',
 	'RT_PAGE_NUMBER'				=> 'Hiljutised teemad lehekülgede kaupa',
-	'RT_PAGE_NUMBER_EXP'			=> 'Saad näidata rohkem teemasi kasutades lehekülges. Sisesta väärtuseks 1, et keelata antud funktsioon. Kui aga sisestad väärtuseks 0, siis kasutatakse nii palju lehekülgi, et näidata kõiki teemasi foorumis (ei ole soovitatav).',
+	'RT_PAGE_NUMBER_EXP'			=> 'Saad näidata rohkem teemasi kasutades lehekülgesi. Sisesta väärtuseks 1, et keelata antud funktsioon. Kui aga sisestad väärtuseks 0, siis kasutatakse nii palju lehekülgi, et näidata kõiki teemasi foorumis (ei ole soovitatav).',
 	'RT_PARENTS'					=> 'Näita vanem foorumit',
 	'RT_PARENTS_EXP'				=> 'Näitab vanem foorumit hiljutistes teemades.',
 	'RT_UNREADONLY'					=> 'Näita ainult lugemata teemasi',
-	'RT_UNREADONLY_EXP'				=> 'Lubades selle, näidatakse ainult lugemata teemasi (ükskõik kas nad on "hiljutised" või mitte). Antud funktsioon kasutab samu seadeid (va foorumid/teemad jne.) nagu tavaline vaadegie. Märkus: see töötab ainult registreeritud kasutajatel; külalised näevad ikka tavalist vaadet.',
+	'RT_UNREADONLY_EXP'				=> 'Lubades selle, näidatakse ainult lugemata teemasi (ükskõik kas nad on "hiljutised" või mitte). Antud funktsioon kasutab samu seadeid (va foorumid/teemad jne.) nagu tavaline vaadegi. Märkus: see töötab ainult registreeritud kasutajatel; külalised näevad ikka tavalist vaadet.',
 
 	'RT_VIEW_ON'					=> 'Näita hiljutistes teemades:',
 ));
