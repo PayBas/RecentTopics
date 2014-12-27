b6aca0e275ddca0a0524632cf4d6ebc73c2ec3bb
Raphaël <Galixte@users.noreply.github.com>
2014-12-27 01:58:14 +0100

    Update for FRENCH translation for info_acp_recenttopics.php
    Update for FRENCH translation for info_acp_recenttopics.php
    This update is also for According to VSEphpbb's directives : https://github.com/Galixte/dbtool/commit/6cb7135#commitcomment-9056827

--- language/fr/info_acp_recenttopics.php
+++ language/fr/info_acp_recenttopics.php
@@ -6,7 +6,7 @@
 * @copyright (c) 2014 PayBas
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
-* French translation by ForumsFaciles (http://www.forumsfaciles.fr)
+* French translation by ForumsFaciles (http://www.forumsfaciles.fr) &  Galixte (http://www.galixte.com)
 *
 * Based on the original NV Recent Topics by Joas Schilling (nickvergessen)
 *
@@ -23,22 +23,22 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'RECENT_TOPICS'					=> 'Sujets récents',
-	'RECENT_TOPICS_LIST'			=> 'Afficher les "sujets récents"',
-	'RECENT_TOPICS_LIST_EXPLAIN'	=> 'Activer pour afficher les sujets de ce forum dans l’Extension "sujets récents".',
+	'RECENT_TOPICS_LIST'			=> 'Afficher les “sujets récents”',
+	'RECENT_TOPICS_LIST_EXPLAIN'	=> 'Activer pour afficher les sujets de ce forum dans l’extension “sujets récents”.',
 
 	'RT_CONFIG'						=> 'Configuration',
 	'RT_ANTI_TOPICS'				=> 'Sujets exclus',
-	'RT_ANTI_TOPICS_EXP'			=> 'Séparés par ", " (exemple: "7, 9")<br />Si vous ne souhaitez exclure aucun sujet, entrez simplement 0.',
-	'RT_MIN_TOPIC_LEVEL'			=> 'Minimum topic type level',
-	'RT_MIN_TOPIC_LEVEL_EXP'		=> 'Determines the minimum level of the topic type to display. It will display topics of the set level, and higher.<br />(0 = normal, 1 = sticky, 2 = announcement, 3 = global announcement)',
+	'RT_ANTI_TOPICS_EXP'			=> 'Saisissez les ID des sujets séparés par : “, ” (exemple : “7, 9”).<br />Si vous ne souhaitez pas exclure de sujet saisissez simplement 0.',
+	'RT_MIN_TOPIC_LEVEL'			=> 'Niveau minimum du type de sujet',
+	'RT_MIN_TOPIC_LEVEL_EXP'		=> 'Détermine le niveau minimum du type de sujet à afficher. Cela affichera les sujets correspondant au niveau paramétré et de niveau supérieur.<br />(0 = Normal, 1 = Post-it, 2 = Annonce, 3 = Annonce globale)',
 	'RT_NUMBER'						=> 'Sujets récents',
 	'RT_NUMBER_EXP'					=> 'Nombre de sujets à afficher sur l’index.',
 	'RT_PAGE_NUMBER'				=> 'Nombre de pages pour les sujets récents',
-	'RT_PAGE_NUMBER_EXP'			=> 'Vous pouvez afficher davantage de sujets récents en utilisant la pagination. Entrez seulement 1 pour désactiver cette option. Si vous sntrez 0, il y aura autant de pages que nécessaire pour afficher tous les sujets de votre forum (non conseillé).',
-	'RT_PARENTS'					=> 'Afficher les forums-parents',
-	'RT_PARENTS_EXP'				=> 'Afficher les forums-parents dans la liste des sujets récents.',
-	'RT_UNREADONLY'					=> 'Afficher uniquement les sujets non-lus',
-	'RT_UNREADONLY_EXP'				=> 'Activer cela pour n’afficher que les sujets non-lus (qu’ils soient récents ou non). Cette fonctionnalité utilise les mêmes paramètres (en excluant les forums/sujets, etc.) que le mode normal. Info: cela ne fonctionnera que pour les utilisateurs connectés; les invités verront la liste normale.',
+	'RT_PAGE_NUMBER_EXP'			=> 'Vous pouvez afficher davantage de sujets récents en utilisant la pagination. Saisissez seulement 1 pour désactiver cette option. Si vous saisissez 0, il y aura autant de pages que nécessaire pour afficher tous les sujets de votre forum (non recommandé).',
+	'RT_PARENTS'					=> 'Afficher les forums parents',
+	'RT_PARENTS_EXP'				=> 'Afficher les forums parents dans la liste des sujets récents.',
+	'RT_UNREADONLY'					=> 'Afficher uniquement les sujets non lus',
+	'RT_UNREADONLY_EXP'				=> 'Vous pouvez afficher uniquement les sujets non lus (qu’ils soient “récents” ou non). Cette fonctionnalité utilise les mêmes paramètres que le mode normal (en excluant les forums / sujets, etc…). Note : Cela fonctionne uniquement pour les utilisateurs connectés; les invités verront la liste normale.',
 
-	'RT_VIEW_ON'					=> 'Afficher les sujets récents sur:',
+	'RT_VIEW_ON'					=> 'Afficher les sujets récents sur :',
 ));
